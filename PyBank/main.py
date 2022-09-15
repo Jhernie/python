c7f4767876d16550885ce967cfdef303b0abcea1
Jhernie <evangelista.jhernie@gmail.com>
2022-09-15 09:13:03 -1000

    Homework progress after tutoring session and study group

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,42 +10,54 @@ with open(csvpath) as BudgetCSV:
     months = []
     ProfitLoss = []
 
-    for i in csvreader:
+    for row in csvreader:
         # print(i[0]) 
-        months.append(i[0])
-        ProfitLoss.append(i[1])
-
-    ProfitLossInt = [round(float(i)) for i in ProfitLoss]
+        months.append(row[0])
+        ProfitLoss.append(float(row[1]))
+    
+    # NO NEED #ProfitLossInt = [round(float(i)) for i in ProfitLoss]
     # print(ProfitLossInt)
 
     ##count total number of months included in the dataset
-    # print(len(months))
-    # print(len(ProfitLossInt))
+    print(len(months))
+    print(sum(ProfitLoss))
 
     ##perform arithmetic on this isolated p&l list
 
-    NetPL = sum(ProfitLossInt)
+    #NetPL = sum(ProfitLossInt)
     # print(NetPL)
 
     ##create the average for p&L
 
-    average = NetPL/len(ProfitLossInt)
-    print(average)
+    # average = NetPL/len(ProfitLossInt)
+    # print(average)
+
+    plChange = []
+
+    for i in range(len(ProfitLoss)-1):
+        plChange.append(ProfitLoss[i + 1] - ProfitLoss[i])
+
+    average = sum(plChange)/len(plChange)
+    print(f'Average Change:$ {average}')
+
+    GreatestedIncrease = max(plChange)
+    print(f'Greatest Increase in Profits: {months}, {GreatestedIncrease}')
+
+    GreatestDecrease = min(plChange)
+    print(f'Greatest Decrease in Profits: {months}, {GreatestDecrease}')
 
 
 
 
-
-
-        
-
+## NOW NEED TO EXPORT TO TXT FILE, and also figure out how to 
+    ##reference the specific month..might need a new variable?
 
 
 #create a calcultion fro the differences from month to month
 
 #when we perfomr that calculation, think about how to change the loop so that the loop stops at the end
 
-# if i == (i - 1): 
+# 
 
     
 
