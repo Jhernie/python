49494ab5f9792cf476268ab138165345db0a9d19
Jhernie <evangelista.jhernie@gmail.com>
2022-09-15 13:33:51 -1000

    homework second half update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,31 @@
 import os
 import csv
 
+pathwayCSV = os.path.join('..','PyPoll','Resources','election_data.csv')
+
+with open(pathwayCSV) as PollingCSV:
+    csvreader = csv.reader(PollingCSV, delimiter=',')
+    
+    #for rows ins csv readers print (rows) -- this shows that the reader works
+            #BUT DON'T USE FILE IS HUGE
+
+#dict - word counters javac
+#Initialize dictionalary 
+#for each candidate check if thy're in there (if in )
+#else not in there intitialze it to 1
+keys_dict = {}
+
+    for row in csvreader:
+        #pull out the desired values row[2] and add it to the dictionary
+
+#check all the rows to see if the name doesn't exist, 
+
+        
+
+    #print(BallotCol)
+
+#we want to make a dictionary for specific values, candidate : total # of votes
+        
+        
+        
+
