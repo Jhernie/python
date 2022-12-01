7363a099608793543adcb0a8eb5510de1188c4ee
Jhernie Evangelista <evangelista.jhernie@gmail.com>
2022-12-01 13:46:29 -0600

    completing old homework assginment after working through more of the issue. Amending both PyPoll and PyBank

--- PyPoll/pypoll.py
+++ PyPoll/pypoll.py
@@ -1,3 +1,4 @@
+
 ##import dependencies
 import os
 import csv
@@ -12,48 +13,78 @@ with open(pathwayCSV) as PollingCSV:
     next(csvreader, None)
     
     ##create empty lists and dict to append data to
-    total_votes = []
+    votes = []
     candidates = []
-    candidate_votes = []
-    
+    results_counter = {}
+
+    ##write loop that creates a counter and appends info to lists and dict
     for row in csvreader:
-        total_votes.append(row[0])
+        votes.append(row[0])
         if row[2] not in candidates:
                 candidates.append(row[2])
-                index = candidates.index(row[2])
-                candidate_votes.append(1)
+                tally = candidates.index(row[2])
+                results_counter[tally] = 1
         else:
-            index = candidates.index(row[2])
-            candidate_votes[index] += 1
+            tally = candidates.index(row[2])
+            results_counter[tally] = results_counter[tally] + 1
 
-    ##create empty dictionary to append data to
-    results = {}
-
-    ##check if that votes (key) is in the dictionary. if true then add one to the existing key. 
-            # if false then add key to dictionary with a value of 1
+    ##change keys in dictionary
+    results_counter["Charles Casper Stockham"] = results_counter.pop(0)
+    results_counter["Diana DeGette"] = results_counter.pop(1)
+    results_counter["Raymon Anthony Doane"] = results_counter.pop(2) 
     
-    for tally in total_votes:
-        if tally in results.keys():
-            results[tally] = results[tally] + 1
-        else:
-            results[tally] = 1
-
     ##define variables
-    vote_count = len(total_votes)
+    vote_count = len(votes)
+        #access the key where the value within the dict is highest
+    winner = max(results_counter, key=results_counter.get)
 
+    ##print result summary
     print("Election Results")
     print("_"*25)
     print()
     print(f'Total Votes: {vote_count}')
     print("_"*25)
-    print(results)
+    print()
+    for candidate, tally in results_counter.items():
+        message1 = f"{candidate}: {round(((tally/vote_count)*100),3)}% ({tally})"
+        message2 = f"Winner: {winner}"
+        print(message1)
+    print("_"*25)
+    print()
+    print(message2)
+    
+##export result summary to txt file
 
 with open("pypoll_analysis.txt", "w") as f:
     print("Election Results", file = f)
     print("_"*25, file = f)
+    print(" ", file = f)
     print(f'Total Votes: {vote_count}', file = f)
     print("_"*25, file = f)
-    print(results, file = f)
+    print(" ", file = f)
+    for candidate, tally in results_counter.items():
+        message1 = f"{candidate}: {round(((tally/vote_count)*100),3)}% ({tally})"
+        message2 = f"Winner: {winner}"
+        print(message1, file = f)
+    print("_"*25, file = f)
+    print(" ", file = f)
+    print(message2, file = f)
+
+
+#     print("Election Results", file = f)
+#     print("_"*25, file = f)
+#     print(f'Total Votes: {vote_count}', file = f)
+#     print("_"*25, file = f)
+#     #print(results_counter, file = f)
+#     file.write(f"{message2}\n")
+
+
+
+
+
+
+
+
 
 
 
@@ -62,6 +93,22 @@ with open("pypoll_analysis.txt", "w") as f:
 
 
 ###____________CODE_GRAVEYARD____________###
+     
+     #print(results_counter)
+     
+     
+     
+        ##check if that votes (key) is in the dictionary. if true then add one to the existing key. 
+            # if false then add key to dictionary with a value of 1
+    
+    # for tally in results_counter:
+    #     if tally in results_counter():
+    #         results_counter[tally] = results_counter[tally] + 1
+                #OR results_counter[tally] += 1
+    #     else:
+    #         results_counter[tally] = 1
+    
+    
     #another way to count votes
     # for count in votes:
     #     if count not in votes:
